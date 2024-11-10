4f3934f6338e53ccf975d5ffb6cd359af4bdf384
bella <146038406+isaahu@users.noreply.github.com>
2024-11-10 11:14:35 -0500

    Add files via upload

---
+++ Fighter Planes F24/Assets/Scripts/MovingObject(edited).cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public int myType;
+    private float coinLifespan = 5f; // Lifetime of the coin in seconds
+    private float coinTimer; // Timer to track how long the coin has been active
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinTimer = coinLifespan; // Initialize timer with the lifespan of the coin
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if this object is a coin
+        if (myType == 4)
+        {
+            // Reduce coin timer by the passed time since last frame
+            coinTimer -= Time.deltaTime;
+
+            // If coin timer reaches zero, destroy the coin
+            if (coinTimer <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    
+    }
+}
